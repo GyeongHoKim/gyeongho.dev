28661ee6e8cd0eda9c715ad243a6fcb2507465af
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-10-02 22:36:59 +0900

    refactor: 적절하지 않은 네이밍, 복잡성 증가로 ScrollText 제거

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,34 @@
 import { Physics } from "@react-three/rapier";
 import { EmployeeCard } from "./EmployeeCard.tsx";
-import { ScrollText } from "./ScrollText.tsx";
+import { useThree } from "@react-three/fiber";
+import { Text } from "@react-three/drei";
 
 export function About() {
+  const { width, height } = useThree((state) => state.viewport);
+
   return (
     <>
       <Physics interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
         <EmployeeCard />
       </Physics>
-      <ScrollText label="I Build" ratioX={-0.45} ratioY={-0.45} />
-      <ScrollText label="Web Experience" ratioX={-0.45} ratioY={-0.35} />
+      <Text
+        color="white"
+        anchorX="left"
+        anchorY="top"
+        position={[width * -0.45, -height * -0.45, -1]}
+        fontSize={0.5}
+      >
+        I Build
+      </Text>
+      <Text
+        color="white"
+        anchorX="left"
+        anchorY="top"
+        position={[width * -0.45, -height * -0.35, -1]}
+        fontSize={0.5}
+      >
+        Web Experience
+      </Text>
     </>
   );
 }
