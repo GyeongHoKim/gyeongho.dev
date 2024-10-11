c867da6a3d802b3c9227b62eac0ef64b629bc992
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-10-11 20:55:13 +0900

    feat: Word & WordCloud component added
     - skill 항목을 표현하기 위한 컴포넌트
     - 글자가 구름처럼 떠다니는 효과를 제공

---
+++ src/components/Word.tsx
@@ -0,0 +1,50 @@
+import * as THREE from "three";
+import { useEffect, useRef, useState } from "react";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
+import { Billboard, BillboardProps, Text } from "@react-three/drei";
+
+export function Word({ children, ...props }: BillboardProps) {
+  const color = new THREE.Color();
+  const fontProps = {
+    fontSize: 0.3,
+    letterSpacing: -0.05,
+    lineHeight: 1,
+    "material-toneMapped": false,
+  };
+  const ref = useRef<Text>(null);
+  const [hovered, setHovered] = useState(false);
+  const hover = (e: ThreeEvent<PointerEvent>) => {
+    e.stopPropagation();
+    setHovered(true);
+  };
+  const unhover = () => {
+    setHovered(false);
+  };
+  useEffect(() => {
+    if (hovered) {
+      document.body.style.cursor = "pointer";
+    }
+    return () => {
+      document.body.style.cursor = "auto";
+    };
+  }, [hovered]);
+
+  useFrame(() => {
+    ref.current?.material.color.lerp(
+      color.set(hovered ? "#fa2720" : "white"),
+      0.1,
+    );
+  });
+
+  return (
+    <Billboard {...props}>
+      <Text
+        ref={ref}
+        onPointerOver={hover}
+        onPointerOut={unhover}
+        children={children}
+        {...fontProps}
+      />
+    </Billboard>
+  );
+}
