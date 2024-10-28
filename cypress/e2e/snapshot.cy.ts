bcda4d442e9651c7c90bbdaa175d0816ccac1585
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-10-28 22:01:32 +0900

    fix: fix E2E test scenario
     - use cypress/browsers:latest image for E2E test
     - added NGINX server docekr for E2E testing(원래는 staging 서버를 구축하는게 맞음)
     - fixed snapshot.cy.ts to apply different url on Environment(local, staging)
    
    described in #15

--- cypress/e2e/snapshot.cy.ts
+++ cypress/e2e/snapshot.cy.ts
@@ -1,30 +1,39 @@
 describe("스냅샷 테스트들", () => {
+  let url: string;
+
+  beforeEach(() => {
+    url = {
+      local: "http://localhost:5173",
+      staging: "http://localhost:3000",
+    }[Cypress.env("TARGET") as "local" | "staging"];
+  });
+
   it("Skills Tab", () => {
-    cy.visit("https://gyeongho.dev");
+    cy.visit(url);
     cy.get('[aria-label="skills"]').click();
     cy.percySnapshot("Skills Tab");
   });
 
   it("Experience Tab", () => {
-    cy.visit("https://gyeongho.dev");
+    cy.visit(url);
     cy.get('[aria-label="experience"]').click();
     cy.percySnapshot("Experience Tab");
   });
 
   it("Projects Tab", () => {
-    cy.visit("https://gyeongho.dev");
+    cy.visit(url);
     cy.get('[aria-label="projects"]').click();
     cy.percySnapshot("Projects Tab");
   });
 
   it("Awards Tab", () => {
-    cy.visit("https://gyeongho.dev");
+    cy.visit(url);
     cy.get('[aria-label="awards"]').click();
     cy.percySnapshot("Awards Tab");
   });
 
   it("Employee Card Tab", () => {
-    cy.visit("https://gyeongho.dev");
+    cy.visit(url);
     cy.get('[aria-label="employee-card"]').click();
     cy.percySnapshot("Employee Card Tab");
   });
